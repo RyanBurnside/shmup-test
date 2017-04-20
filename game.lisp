fb1269d924b0d9717e2bb67189216210c8b858fe
Ryan Burnside <pixeloutlaw@gmail.com>
2017-04-20 00:31:41 -0600

    Adding game engine functionality, moving test functions into engine

--- game.lisp
+++ game.lisp
@@ -4,6 +4,19 @@
 ;;;; state changes withing the game
 
 
+;; Accessory defuns
+(defun burst-fire (x y num-shots aim-direction spread recipient-function)
+  "burst-fire will pass the valuex x, y, direction, speed into 
+recipient-function,"
+  (if (< num-shots 1) (return-from burst-fire))
+  (when (= num-shots 1) ; 1 or less shots
+    (funcall recipient-function x y aim-direction)
+    (return-from burst-fire))
+  (let ((start-angle (- aim-direction (* spread .5)))
+	(step-angle (/ spread (1- (* 1.0 num-shots)))))
+	(dotimes (i num-shots)
+	  (funcall recipient-function x y (+ start-angle (* step-angle i))))))
+
 (defclass game ()
   ((state        :accessor game-state :initform 'title)
 
@@ -27,7 +40,9 @@
 								:center-y 320
 								:width 480
 								:height 640)
-	      :documentation "Hitbox for defining play area")))
+	      :documentation "Hitbox for defining play area")
+   (player-image :accessor game-player-image)
+   (bullet-image :accessor game-bullet-image)))
 
 (defmethod initialize-instance :after ((game game) 
 				       &key 
@@ -38,7 +53,36 @@
 		       :width width 
 		       :height height
 		       :center-x (/ width 2)
-		       :center-y (/ height 2))))
+		       :center-y (/ height 2)))
+  (dotimes (i 4)
+    (add-playerf game
+		 (make-instance 'player 
+				:x (random (width game))
+				:y (random (height game)))))
+
+  ;; Should always be the last function executed in init as it starts SDL
+  (sdl:load-library) ;; DO NOT APPEND BELOW THIS LINE
+  (sdl:with-init () (sdl:window 
+		     (hitbox-width (game-play-area game))
+		     (hitbox-height (game-play-area game ))
+		     :title-caption "Shmup Test"
+		     :DOUBLE-BUFFER T)
+		 (setf (sdl:frame-rate) 60)
+		 (setf (game-bullet-image game)
+		       (sdl-image:load-image 
+			"Resources/Sprites/test_shot.png" 
+			:image-type :PNG :force t :color-key-at #(0 0)))
+		 
+		 (setf (game-player-image game)
+		       (sdl-image:load-image 
+			"Resources/Sprites/ships.gif" 
+			:image-type :GIF :force t :color-key-at #(0 0)))
+		 (sdl:with-events ()
+		   (:quit-event () t)
+		   (:idle ()
+			  (sdl:clear-display sdl:*black*)
+			  (stepf game)
+			  (sdl:update-display)))))
 
 ;;; getters
 (defmethod width ((game game))
@@ -61,8 +105,54 @@
 (defmethod add-player-shotf ((game game) (player-shot game-object))
   (vector-push-extend player-shot (game-player-shots game)))
 
+(defmethod burst-newf ((game game) x y direction)
+  (add-enemy-shotf game
+		   (make-game-object (floor (/ (width game) 2))
+				     (floor (* (height game) .33))
+				     :speed 5.0
+				     :direction direction
+				     :width 16
+				     :height 16)))
 
+(defmethod player-shootf ((game game) x y direction)
+  (add-player-shotf game
+		    (make-game-object x
+				      y
+				      :speed 10.0
+				      :direction direction
+				      :width 16
+				      :height 16)))
+
+
+(defmethod draw-gamef ((game game))
+  "Main method for drawing game"
+  (let ((pt (sdl:point)))
+    (loop for i across (game-enemy-shots game) 
+       do
+	 (setf (aref pt 0) (floor (mover-x i)))
+	 (setf (aref pt 1) (floor (mover-y i)))
+	 (sdl:draw-surface-at (game-bullet-image game) pt))
+    (loop for i across (game-player-shots game) 
+       do
+	 (setf (aref pt 0) (floor (mover-x i)))
+	 (setf (aref pt 1) (floor (mover-y i)))
+	 (sdl:draw-surface-at (game-bullet-image game) pt))
+    (loop for p across (game-players game)
+	 do
+	 (sdl:draw-surface-at-* (game-player-image game)
+				(floor (mover-x p))
+				(floor (mover-y p))))))
+
+(defmethod print-debug ((game game))
+  "Debug method called during keypress"
+  (when (sdl:get-key-state :sdl-key-f1)
+    (format t "player shots: ~a~%" (length (game-player-shots game)))
+    (format t "enemy shots: ~a~%" (length (game-enemy-shots game)))
+    (format t "enemies: ~a~%" (length (game-enemies game)))
+    (format t "players: ~a~%" (length (game-players game)))))
+    
 (defmethod reset-gamef ((game game))
+  "Reset the game to default state"
   (setf (game-state game) 'title)
   (reset-fill-vectorf (game-player-shots game))
   (reset-fill-vectorf (game-enemy-shots game))
@@ -73,30 +163,40 @@
   (setf (fill-pointer vec) 0))
 
 (defmethod stepf ((game game))
-  ;; move 
+  "Main step function for the game"
+  (print-debug game)
   (with-accessors ((players game-players)
 		   (player-shots game-player-shots)
 		   (enemies game-enemies)
-		   (enemy-shots game-enemy-shots))
-                  game
+		   (enemy-shots game-enemy-shots)
+		   (play-area game-play-area))
+      game
+    (burst-fire (* (width game) .5)
+		(* (height game) .5)
+		1
+		(random (* 2 PI))
+		1
+		(lambda (x y direction) 
+		  (burst-newf game x y direction)))
+
     (loop for i across players 
        do (unless (game-object-dead i)
 	    (stepf i)))
     (loop for i across player-shots 
        do
-	 (when (not (collidep (game-play-area game) (get-hitbox i)))
+	 (when (not (collidep play-area (get-hitbox i)))
 	   (setf (game-object-dead i) t))
 	 (unless (game-object-dead i)
 	    (stepf i)))
     (loop for i across enemies 
        do
-	 (when (not(collidep (game-play-area game) (get-hitbox i)))
+	 (when (not(collidep play-area (get-hitbox i)))
 	   (setf (game-object-dead i) t))
 	 (unless (game-object-dead i)
 	    (stepf i)))
     (loop for i across enemy-shots 
        do
-	 (when (not (collidep (game-play-area game) (get-hitbox i)))
+	 (when (not (collidep play-area (get-hitbox i)))
 	   (setf (game-object-dead i) t))
 	 (unless (game-object-dead i)
 	    (stepf i)))
@@ -107,5 +207,6 @@
     ;; cull dead shots and enemies
     (setf enemies (delete-if #'game-object-dead enemies))
     (setf enemy-shots (delete-if #'game-object-dead enemy-shots))
-    (setf player-shots (delete-if #'game-object-dead player-shots))))
+    (setf player-shots (delete-if #'game-object-dead player-shots)))
+  (draw-gamef game))
     
--- shmup-test.lisp
+++ shmup-test.lisp
@@ -7,43 +7,6 @@
 
 (defparameter *width* 240)
 (defparameter *height* 320)
-(defparameter *center-width* (/ *width*  2))
-(defparameter *center-height* (/ *height*  2))
-(defparameter *objects* '())
-
-(defparameter *engine* (make-instance 'game :width *width* :height *height*))
-
-
-(defun burst-fire (x y num-shots aim-direction spread recipient-function)
-  "burst-fire will pass the valuex x, y, direction, speed into 
-recipient-function,"
-  (if (< num-shots 1) (return-from burst-fire))
-  (when (= num-shots 1) ; 1 or less shots
-    (funcall recipient-function x y aim-direction)
-    (return-from burst-fire))
-  (let ((start-angle (- aim-direction (* spread .5)))
-	(step-angle (/ spread (1- (* 1.0 num-shots)))))
-	(dotimes (i num-shots)
-	  (funcall recipient-function x y (+ start-angle (* step-angle i))))))
- 
-(defun burst-new (x y direction)
-  (add-enemy-shotf *engine*
-		   (make-game-object *center-width*
-				     (floor (* *height* .33))
-				     :speed 5.0
-				     :direction direction
-				     :width 16
-				     :height 16)))
-
-(defun player-shoot (x y direction)
-  (add-player-shotf *engine* 
-		    (make-game-object x
-				      y
-				      :speed 10.0
-				      :direction direction
-				      :width 16
-				      :height 16)))
-
 (defun game-update ()
   (tickf *direction-ticker*)
   (burst-fire *center-width* 
@@ -67,60 +30,7 @@ recipient-function,"
 		#'player-shoot))
   (stepf *engine*))
 
-
-
-(defparameter *shot-direction* 0.0)
-(defparameter *direction-ticker* (make-instance 'ticker-action
-					       :ready-at 1
-					       :on-ready
-					       (lambda ()
-						 (incf *shot-direction*
-						       (/ PI 10.0)))))
-
-
-(defun game-draw ()
-  (let ((pt (sdl:point)))
-    (loop for i across (game-enemy-shots *engine*) 
-       do
-	 (setf (aref pt 0) (floor (mover-x i)))
-	 (setf (aref pt 1) (floor (mover-y i)))
-	 (sdl:draw-surface-at *bullet-image* pt))
-    (loop for i across (game-player-shots *engine*) 
-       do
-	 (setf (aref pt 0) (floor (mover-x i)))
-	 (setf (aref pt 1) (floor (mover-y i)))
-	 (sdl:draw-surface-at *bullet-image* pt))
-    (sdl:draw-surface-at-* *player-image* (sdl:mouse-x)
-			 (sdl:mouse-y))))
-
-
-(defparameter *bullet-image* nil)
-(defparameter *player-image* nil)
-
 (defun shmup-test ()
-  ;; Make sure the libraries are loaded on startup.
-  ;; Necessary when creating a stand-alone executable.
-  (sdl:load-library)
-  (setf *objects* '())
-  (sdl:with-init () (sdl:window 
-		     (width *engine*)
-		     (height *engine* )
-		     :title-caption "Shmup Test"
-		     :DOUBLE-BUFFER T)
-		 (setf (sdl:frame-rate) 60)
-		 (setf *bullet-image* 
-		       (sdl-image:load-image 
-			"Resources/Sprites/test_shot.png" 
-			:image-type :PNG :force t :color-key-at #(0 0)))
-
-		 (setf *player-image* 
-		       (sdl-image:load-image 
-			"Resources/Sprites/ships.gif" 
-			:image-type :GIF :force t :color-key-at #(0 0)))
-		 (sdl:with-events ()
-		   (:quit-event () t)
-		   (:idle ()
-			  (sdl:clear-display sdl:*black*)
-			  (game-update)
-			  (game-draw)
-			  (sdl:update-display)))))
+  ;; A single instance of game starts the whole program
+  (make-instance 'game :width *width* :height *height*)
+ )
